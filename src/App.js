15b903c4f69297a0fad6e95090c4e5353ad650cf
Emilian Con <emiliancon@Emilians-MacBook-Pro-2.local>
2023-06-04 02:46:57 +0100

    Initial init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+
+import React, { useState, useEffect } from 'react';
+import { Button, Form, Container, Row, Col, Modal } from 'react-bootstrap';
 
 function App() {
+  const [name, setName] = useState('');
+  const [showModal, setShowModal] = useState(true);
+  const [isLandscape, setIsLandscape] = useState(window.innerWidth > window.innerHeight);
+
+  const handleChange = (event) => {
+    setName(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    window.screen.orientation.lock('landscape');
+  }
+
+  const handleDelete = () => {
+    setName('');
+    window.screen.orientation.lock('portrait');
+  }
+
+  const handleClose = () => setShowModal(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLandscape(window.innerWidth > window.innerHeight);
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid style={{ backgroundColor: '#000000', height: '100vh' }}>
+      {isLandscape ? (
+        <Row className="justify-content-center align-items-center" style={{ color: '#ffffff', fontSize: '10vw', height: '100vh', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>
+          {name}
+        </Row>
+      ) : (
+        <>
+          <Row>
+            <Col>
+              <h1>Passenger Name Display</h1>
+            </Col>
+          </Row>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group>
+              <Form.Label>Passenger Name</Form.Label>
+              <Form.Control type="text" value={name} onChange={handleChange} />
+            </Form.Group>
+            <Button type="submit">Submit</Button>
+          </Form>
+        </>
+      )}
+      <Row className="fixed-bottom">
+        <Col>
+          <Button variant="danger" onClick={handleDelete}>Delete</Button>
+        </Col>
+      </Row>
+      <Modal show={showModal} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>How to Use</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Enter the passenger's name and submit. The screen will automatically flip to landscape mode and display the name. To delete the name, click the Delete button.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </Container>
   );
 }
 
